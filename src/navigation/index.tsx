3ed0783a9a437e13b77e4f36237d72480b50e18d
Noel-S <noel.silva.v@gmail.com>
2022-05-15 14:42:46 -0500

    Creating structure for the project
    Adding neccesary folders for maintaining the project files ordered, and installing react native navigation dependencies

---
+++ src/navigation/index.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import HomeScreen from '../screens/home';
+import DetailScreen from '../screens/detail';
+
+const Stack = createNativeStackNavigator();
+
+const Navigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: 'Welcome'}}
+        />
+        <Stack.Screen name="Detail" component={DetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Navigation;
