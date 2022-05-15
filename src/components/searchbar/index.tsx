a2547efb906debcbaa1c5924bd2c29ca7f326e21
Noel-S <noel.silva.v@gmail.com>
2022-05-15 16:25:46 -0500

    Adding support for svg files

--- src/components/searchbar/index.tsx
+++ src/components/searchbar/index.tsx
@@ -1,15 +1,21 @@
 import * as React from 'react';
-import {TextInput, View} from 'react-native';
+import {TextInput, TouchableOpacity, View} from 'react-native';
 import styles from './styles';
+import SearchSVG from '../../assets/icons/search.svg';
+import DictationSVG from '../../assets/icons/dictation.svg';
 
 const SearchBar = () => {
   return (
     <View style={styles.container}>
+      <SearchSVG />
       <TextInput
         style={styles.searchbar}
         placeholder="What do you want to eat?"
         placeholderTextColor={'#fff'}
       />
+      <TouchableOpacity style={styles.dictaionButton}>
+        <DictationSVG />
+      </TouchableOpacity>
     </View>
   );
 };
--- src/components/searchbar/styles.tsx
+++ src/components/searchbar/styles.tsx
@@ -2,17 +2,24 @@ import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection: 'row',
+    alignItems: 'center',
     height: 36,
     width: '100%',
     backgroundColor: 'rgba(142, 142, 147, 0.12)',
     borderRadius: 10,
-    padding: 0,
+    paddingHorizontal: 8,
   },
   searchbar: {
     height: '100%',
     padding: 0,
     fontSize: 17,
     lineHeight: 20,
+    paddingHorizontal: 8,
+    flex: 1,
+  },
+  dictaionButton: {
+    width: 19,
   },
 });
 
